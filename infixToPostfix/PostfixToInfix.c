78601215a048acd6dbcae2101cfb836b68976a47
chaiyanan-s <115109146+chaiyanan-s@users.noreply.github.com>
2023-02-15 21:58:15 +0700

    Add files via upload

--- infixToPostfix/PostfixToInfix.c
+++ infixToPostfix/PostfixToInfix.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
+#define MAX = 100;
 
 typedef struct nd {
     char c;
     struct nd *next;
 }node;
+
 node *top = NULL;
+int count = 0;
 
 void push(char x) {
     node *n = malloc(sizeof(node));
@@ -25,10 +29,39 @@ char pop() {
     return p;
 }
 
-void convert
+void convert(char ch[]) {
+    for (int i = 0; ch[i] != '\0'; i++) {
+	    if (ch[i] >= 'a'&& ch[i] <= 'z'|| ch[i] >= 'A' && ch[i] <= 'Z') {
+            push(i);
+        } else if(ch[i] == '+' || ch[i] == '-' || ch[i] == '*' || ch[i] == '/' ||
+                    ch[i] == '^') {
+            char a =  pop();
+            char b = pop();
+            char p[5];
+            p[0] = '(';
+            p[1] = b;
+            p[2] = ch[i];
+            p[3] = a;
+            p[4] = ')';
+
+            strrev(p);
+            for(int j = 0; j < 5; j++) {
+                push(p[j]);
+            }
+        }
+    }
+
+}
 
 void main() {
     char ch[50];
     scanf("%s", &ch);
     convert(ch);
+
+    for(int i = 0; i < 5; i++) {
+        printf("%c",pop());
+    }
+    
 }
+
+
