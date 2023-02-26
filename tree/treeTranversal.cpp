0e38fccff6d3c8921ec672665cc4eccf50487f1b
chaiyanan-s <115109146+chaiyanan-s@users.noreply.github.com>
2023-02-26 20:49:17 +0700

    Update treeTranversal.cpp

--- tree/treeTranversal.cpp
+++ tree/treeTranversal.cpp
@@ -1,65 +1,61 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
- 
-struct bintree_node{
-    bintree_node *left;
-    bintree_node *right;
-    int data;
+
+/*
+/* ===================================== tree ===================================== */ 
+struct Node {
+    char data;
+    Node *left, *right;
 };
-//----------------------------------------------------------
-/* ========== inner bst class ========== */
 class bst{
-    bintree_node *root;
+    Node* root;
+    stack<Node*> st;
     public: bst(){
         root = NULL;
     }
     int isempty() {
         return(root == NULL);
     }
-    void insert(int item);
+    void insert(string);
     void displayBinTree();
-    void printBinTree(bintree_node *);
-    void traversePostOrder(struct bintree_node *);
-    void traverseInOrder(struct bintree_node *);
-    void traversePreOrder(struct bintree_node *);
+    void traversePostOrder(Node *);
+    void traverseInOrder(Node *);
+    void traversePreOrder(Node *);
      
 };
+
 /* ========== end of bst class ========== */
 //--------------------------------------------------------
 
-void bst::insert(int item) {
-    bintree_node *p = new bintree_node;
-    bintree_node *parent;
+Node* createNode(char data) {
+    Node* node = new Node;
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
 
-    p->data = item;
-    p->left = NULL;
-    p->right = NULL;
-    parent = NULL;
+void bst::insert(string prefix) {
 
-    if(isempty()) {
-        root = p;
-    } else {
-        bintree_node *ptr;
-        ptr = root;
-        while(ptr != NULL){
-            parent = ptr;
-            if(item > ptr->data) {        
-                ptr = ptr->right;
-            } else {
-                ptr = ptr->left;
-            }
+    for (int i = prefix.size() - 1; i >= 0; i--) {
+        char c = prefix[i];
+        if (isdigit(c) || isalpha(c)) {
+            Node* node = createNode(c);
+            st.push(node);
         }
-
-        if(item<parent->data){
-            parent->left=p;
-        } else {
-            parent->right=p;
+        else {
+            Node* node = createNode(c);
+            node->left = st.top();
+            st.pop();
+            node->right = st.top();
+            st.pop();
+            st.push(node);
         }
     }
+
+    root =  st.top();
 }
 
 void bst::displayBinTree(){
-    printBinTree(root);
     cout << "\n pre-order traversal: ";
     traversePreOrder(root);
     cout << "\n  in-order traversal: ";
@@ -67,50 +63,152 @@ void bst::displayBinTree(){
     cout << "\npost-order traversal: ";
     traversePostOrder(root);
 }
-void bst::printBinTree(bintree_node *ptr){
-    if(ptr  !=  NULL){
-        printBinTree(ptr->left);
-        cout << " " << ptr->data << " "; 
-        printBinTree(ptr->right);
-    }
-}
 
-void bst::traversePreOrder(struct bintree_node *temp) {
+/*====== TRAVERSE* ======*/
+void bst::traversePreOrder(Node *temp) {
     if (temp != NULL) {
-        cout << " " << temp->data << " ";
+        cout << temp->data;
         traversePreOrder(temp->left);
         traversePreOrder(temp->right);
     }
 }
-
-// Traverse Inorder
-void bst::traverseInOrder(struct bintree_node *temp) {
+void bst::traverseInOrder(Node *temp) {
     if (temp != NULL) {
         traverseInOrder(temp->left);
-        cout << " " << temp->data << " ";
+        cout << temp->data;
         traverseInOrder(temp->right);
     }
 }
-
-// Traverse Postorder
-void bst::traversePostOrder(struct bintree_node *temp) {
+void bst::traversePostOrder(Node *temp) {
     if (temp != NULL) {
         traversePostOrder(temp->left);
         traversePostOrder(temp->right);
-        cout << " " << temp->data << " ";
+        cout << temp->data;
   }
 }
+/*====== TRAVERSE* ======*/
+/* ===================================== tree ===================================== */
 
-int main(){
+
+/* ===================================== mutanial ===================================== */
+
+bool isOperator(char c) {
+    return (!isalpha(c) && !isdigit(c));
+}
+int getPriority(char C) {
+	if (C == '-' || C == '+')
+		return 1;
+	else if (C == '*' || C == '/')
+		return 2;
+	else if (C == '^')
+		return 3;
+	return 0;
+}
+string infixToPostfix(string infix) {
+	infix = '(' + infix + ')';
+	int l = infix.size();
+	stack<char> char_stack;
+	string output;
+
+	for (int i = 0; i < l; i++) {
+
+		// If the scanned character is an
+		// operand, add it to output.
+		if (isalpha(infix[i]) || isdigit(infix[i]))
+			output += infix[i];
+
+		// If the scanned character is an
+		// ‘(‘, push it to the stack.
+		else if (infix[i] == '(')
+			char_stack.push('(');
+
+		// If the scanned character is an
+		// ‘)’, pop and output from the stack
+		// until an ‘(‘ is encountered.
+		else if (infix[i] == ')') {
+			while (char_stack.top() != '(') {
+				output += char_stack.top();
+				char_stack.pop();
+			}
+
+			// Remove '(' from the stack
+			char_stack.pop();
+		}
+
+		// Operator found
+		else
+		{
+			if (isOperator(char_stack.top()))
+			{
+				if(infix[i] == '^')
+				{
+					while (getPriority(infix[i]) <= getPriority(char_stack.top()))
+					{
+						output += char_stack.top();
+						char_stack.pop();
+					}
+					
+				}
+				else
+				{
+					while (getPriority(infix[i]) < getPriority(char_stack.top()))
+					{
+						output += char_stack.top();
+						char_stack.pop();
+					}
+					
+				}
+
+				// Push current Operator on stack
+				char_stack.push(infix[i]);
+			}
+		}
+	}
+	while(!char_stack.empty()){
+		output += char_stack.top();
+		char_stack.pop();
+	}
+	return output;
+}
+
+string infixToPrefix(string infix)
+{
+	/* Reverse String
+	* Replace ( with ) and vice versa
+	* Get Postfix
+	* Reverse Postfix * */
+	int l = infix.size();
+
+	// Reverse infix
+	reverse(infix.begin(), infix.end());
+
+	// Replace ( with ) and vice versa
+	for (int i = 0; i < l; i++) {
+
+		if (infix[i] == '(') {
+			infix[i] = ')';
+		}
+		else if (infix[i] == ')') {
+			infix[i] = '(';
+		}
+	}
+
+	string prefix = infixToPostfix(infix);
+
+	// Reverse postfix
+	reverse(prefix.begin(), prefix.end());
+
+	return prefix;
+}
+
+/* ===================================== mutanial ===================================== */
+
+int main() {
     bst b;
-    b.insert(20);
-    b.insert(10);
-    b.insert(5);
-    b.insert(15);
-    b.insert(40);
-    b.insert(45);
-    b.insert(30); 
-     
-    cout << " Binary tree created: ";
+	string s = "((A+B)*C+(D-E+F)/G)";
+    string exp = infixToPrefix(s);
+    b.insert(exp);
+    cout << infixToPrefix(s);
     b.displayBinTree();
+	return 0;
 }
